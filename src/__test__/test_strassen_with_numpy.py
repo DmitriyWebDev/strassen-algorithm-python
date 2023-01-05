9c09077be60a6534b251cf018258ac68a0f4ad3e
DmitriyWebDev <d-man_real_akkaunt.ru@mail.ru>
2023-01-05 11:30:18 +0600

    Added strassen_with_numpy() and test

---
+++ src/__test__/test_strassen_with_numpy.py
@@ -0,0 +1,32 @@
+import unittest
+from ..strassen_with_numpy import strassen_with_numpy
+import numpy as np
+
+
+class MyTestCase(unittest.TestCase):
+    def test_strassen_with_numpy(self):
+        matrix_a = np.array([
+            [3, 5, 1, 3],
+            [1, 2, 3, 4],
+            [4, 5, 6, 8],
+            [7, 8, 9, 3]
+        ])
+        matrix_b = np.array([
+            [4, 1, 2, 3],
+            [1, 2, 1, 6],
+            [2, 4, 6, 2],
+            [6, 2, 5, 4]
+        ])
+        matrix_result = np.array([
+            [37, 23, 32, 53],
+            [36, 25, 42, 37],
+            [81, 54, 89, 86],
+            [72, 65, 91, 99]
+        ])
+
+        # https://stackoverflow.com/questions/3302949/best-way-to-assert-for-numpy-array-equality
+        np.testing.assert_array_equal(strassen_with_numpy(matrix_a, matrix_b), matrix_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- src/index.py
+++
@@ -1,2 +0,0 @@
-def my_function(some_string: str):
-    return some_string + " Refsnes"
