1dd5dced86a4f632501e0184bf984b15952ee81e
DmitriyWebDev <d-man_real_akkaunt.ru@mail.ru>
2023-01-07 19:39:23 +0600

    Added test for brute_force()

--- src/__test__/test_strassen_with_numpy.py
+++ src/__test__/test_strassen_with_numpy.py
@@ -2,7 +2,7 @@ import unittest
 
 from numpy import array
 
-from ..strassen_with_numpy import strassen_with_numpy, split
+from ..strassen_with_numpy import strassen_with_numpy, split, brute_force
 import numpy as np
 
 
@@ -29,6 +29,29 @@ class MyTestCase(unittest.TestCase):
 
         # https://stackoverflow.com/questions/3302949/best-way-to-assert-for-numpy-array-equality
         np.testing.assert_array_equal(strassen_with_numpy(matrix_a, matrix_b), matrix_result)
+
+    def test_brute_force(self):
+        matrix_a = np.array([
+            [3, 5, 1, 3],
+            [1, 2, 3, 4],
+            [4, 5, 6, 8],
+            [7, 8, 9, 3]
+        ])
+        matrix_b = np.array([
+            [4, 1, 2, 3],
+            [1, 2, 1, 6],
+            [2, 4, 6, 2],
+            [6, 2, 5, 4]
+        ])
+        matrix_result = np.array([
+            [37, 23, 32, 53],
+            [36, 25, 42, 37],
+            [81, 54, 89, 86],
+            [72, 65, 91, 99]
+        ])
+
+        # https://stackoverflow.com/questions/3302949/best-way-to-assert-for-numpy-array-equality
+        np.testing.assert_array_equal(brute_force(matrix_a, matrix_b), matrix_result)
 
     def test_split(self):
         matrix_a = np.array([
